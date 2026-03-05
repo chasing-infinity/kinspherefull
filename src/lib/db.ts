add0cad94c4609d04bae5ead86bb4200509e63be
chasing-infinity <torsha@bipolarfactory.com>
2026-03-06 04:33:13 +0530

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,16 @@
 import { PrismaClient } from "@prisma/client";
-const g = globalThis as unknown as { prisma: PrismaClient | undefined };
-export const db = g.prisma ?? new PrismaClient({ log: process.env.NODE_ENV === "development" ? ["error"] : ["error"] });
-if (process.env.NODE_ENV !== "production") g.prisma = db;
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
+
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error"],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
